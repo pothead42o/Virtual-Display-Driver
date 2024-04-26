7195296a51a7729c9388358db8f5acdb2aaba35c
Bud <75859057+bud3699@users.noreply.github.com>
2024-04-26 06:55:54 +0100

    in progress (DO NOT EDIT)

--- Community Scripts/toggle-VDD.ps1
+++ Community Scripts/toggle-VDD.ps1
@@ -8,7 +8,7 @@
     }
  }
 
-$device = get-pnpdevice -class Display -FriendlyName 'IddSampleDriver Device HDR' -ErrorAction SilentlyContinue
+$device = get-pnpdevice -class Display -FriendlyName 'Virtual Display with HDR' -ErrorAction SilentlyContinue
 
 if ($device) {
     if ($device.Status -eq 'OK') {
